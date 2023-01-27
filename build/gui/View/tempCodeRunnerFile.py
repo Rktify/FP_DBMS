04f7b992bb03b39f43d6aac6064266b89c977731
Rktify <wilbertwirawanichwan@gmail.com>
2023-01-27 08:43:14 +0700

    Fixed

--- build/gui/View/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-cursor.execute("SELECT p.PurchaseID, u.firstName, t.TicketType, s.TicketStatus, e.EventName, e.Location, e.Date, e.Time FROM Purchase p JOIN UserInfo u ON p.UserID = u.UserID JOIN Tickets t ON p.TicketID = t.TicketID JOIN Event e ON e.EventID = p.EventID JOIN TicketStatus s ON t.TicketStatusID = s.TicketStatusID WHERE p.UserID = %s;", (x[0],))
-# print(cursor.fetchall())
